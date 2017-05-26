596e917bfd60c4ed96c56552774f1e0f4dc42624
Ted Ostrem <ted@vincross.com>
2017-05-26 16:30:45 +0800

    tabs

--- FastRTPS.h
+++ FastRTPS.h
@@ -2,29 +2,29 @@
 extern "C" {
 #endif
 
-typedef struct {
-	void* ReliablePublisher;
-	void* MultimediaPublisher;
-} FastRTPSPublisherAttributes; 
+	typedef struct {
+		void* ReliablePublisher;
+		void* MultimediaPublisher;
+	} FastRTPSPublisherAttributes; 
 
-typedef struct {
-	void* Media;
-} FastRTPSTopicDataTypes;
+	typedef struct {
+		void* Media;
+	} FastRTPSTopicDataTypes;
 
-typedef struct {
-	void* wrapper;
-} FastRTPSParticipant;
+	typedef struct {
+		void* wrapper;
+	} FastRTPSParticipant;
 
-typedef struct {
-	void* wrapper;
-} FastRTPSPublisher;
+	typedef struct {
+		void* wrapper;
+	} FastRTPSPublisher;
 
-FastRTPSPublisherAttributes FastRTPSGetPublisherAttributes(void* topicDataType, char* topicName);
-FastRTPSPublisher* FastRTPSNewPublisher(FastRTPSParticipant *participant, void* publisherAttributes);
-void FastRTPSRegisterType(FastRTPSParticipant* participant, void* topicDataType);
-FastRTPSTopicDataTypes FastRTPSGetTopicDataTypes();
-FastRTPSParticipant* FastRTPSNewParticipant(char *name);
-void FastRTPSPublish(FastRTPSPublisher* publisher, char *image);
+	FastRTPSPublisherAttributes FastRTPSGetPublisherAttributes(void* topicDataType, char* topicName);
+	FastRTPSPublisher* FastRTPSNewPublisher(FastRTPSParticipant *participant, void* publisherAttributes);
+	void FastRTPSRegisterType(FastRTPSParticipant* participant, void* topicDataType);
+	FastRTPSTopicDataTypes FastRTPSGetTopicDataTypes();
+	FastRTPSParticipant* FastRTPSNewParticipant(char *name);
+	void FastRTPSPublish(FastRTPSPublisher* publisher, char *image);
 
 #ifdef __cplusplus
 }
--- FastRTPSParticipantWrapper.cxx
+++ FastRTPSParticipantWrapper.cxx
@@ -4,11 +4,11 @@
 using namespace eprosima::fastrtps;
 
 FastRTPSParticipantWrapper::FastRTPSParticipantWrapper(char *name) {
-  ParticipantAttributes PParam;
-  PParam.rtps.builtin.domainId = 0;
-  PParam.rtps.builtin.leaseDuration = c_TimeInfinite;
-  PParam.rtps.setName(name);
-  participant = Domain::createParticipant(PParam);
-  if (participant == nullptr)
+	ParticipantAttributes PParam;
+	PParam.rtps.builtin.domainId = 0;
+	PParam.rtps.builtin.leaseDuration = c_TimeInfinite;
+	PParam.rtps.setName(name);
+	participant = Domain::createParticipant(PParam);
+	if (participant == nullptr)
 		throw;
 }
